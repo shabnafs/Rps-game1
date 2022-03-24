ef130731c6f928c7ea47d28a6e12526bd75bdcd5
syedf <syedf@Heera>
2022-03-23 23:05:38 -0700

    test commit

--- rps-ant-master/src/main/java/com/mcnz/BreakMeMaybe.java
+++ rps-ant-master/src/main/java/com/mcnz/BreakMeMaybe.java
@@ -3,6 +3,7 @@ package com.mcnz;
 public class BreakMeMaybe {
 	
 	// remove the semi-colon in the next line to break the build with a compile error
+	//testing
 	public static final int MAX_COUNT = 0;
 	
 }
